88a93f2a3c87bcf40ca818179c5a57c20684fe91
Nurbolot <nurbolot.boobekovv@gmail.com>
2022-09-24 23:08:05 +0300

    added BookCart

--- src/components/BookCart/BookCart.jsx
+++ src/components/BookCart/BookCart.jsx
@@ -1,26 +1,35 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import style from './bookCart.module.scss'
 
-export const BookCart = ({ handleClick }) => {
+export const BookCart = ({ handleClick, data }) => {
 
-  const id = 1;
+  let { id, title, author, created_at, updated_at } = data;
+  created_at = created_at.slice(0, 10) + ' / ' + created_at.slice(11, 16);
+  updated_at = updated_at.slice(0, 10) + ' / ' + updated_at.slice(11, 16);
 
   return <div className={style.body} onClick={() => {
     handleClick(id)
   }} >
     <div className={style.wrapper}>
       <div className={style.info}>
-        <span> Title </span>War and Peace
+        <span> Title </span>{title}
       </div>
       <div className={style.info}>
-        <span> Author </span>Lev Tolstoi
+        <span> Author </span>{author}
       </div>
 
       <div className={style.date}>
-        <p>Created at: <span> 22.12.01 13:30</span></p>
-        <p>Updated at: <span>22.12.01 15:13</span> </p>
+        <p>Created at:
+          <span>
+            {created_at}
+          </span>
+        </p>
+        <p>Updated at:
+          <span>
+            {updated_at}
+          </span>
+        </p>
       </div>
     </div>
   </div>
