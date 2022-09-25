939b65dec05bc648961dbdf8159ab87e74590166
Nurbolot <nurbolot.boobekovv@gmail.com>
2022-09-25 13:25:21 +0300

    External Server URL connected

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4444/api",
+  baseURL: "https://users-server.onrender.com/api",
 });
 
 export default instance;
